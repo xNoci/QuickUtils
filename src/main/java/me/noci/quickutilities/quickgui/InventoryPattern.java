50f49f091cf41d6fda9bdbb306c19f7c07fef9bb
Noci <nocigit@gmail.com>
2022-09-18 10:58:03 +0200

    Added InventoryPattern

---
+++ src/main/java/me/noci/quickutilities/quickgui/InventoryPattern.java
@@ -0,0 +1,36 @@
+package me.noci.quickutilities.quickgui;
+
+import org.bukkit.inventory.Inventory;
+
+import java.util.stream.IntStream;
+
+public enum InventoryPattern {
+
+    TOP(size -> IntStream.range(0, size).filter(i -> i < 9).toArray()),
+    BOTTOM(size -> IntStream.range(0, size).filter(i -> i > size - 10).toArray()),
+    LEFT(size -> IntStream.range(0, size).filter(i -> i % 9 == 0).toArray()),
+    RIGHT(size -> IntStream.range(0, size).filter(i -> (i + 1) % 9 == 0).toArray()),
+    TOP_LEFT_CORNER(size -> IntStream.range(0, size).filter(i -> i < 2 || i == 9).toArray()),
+    TOP_RIGHT_CORNER(size -> IntStream.range(0, size).filter(i -> i == 7 || i == 8 || i == 17).toArray()),
+    BOTTOM_LEFT_CORNER(size -> IntStream.range(0, size).filter(i -> i == size - 18 || (i > size - 10 && i < size - 7)).toArray()),
+    BOTTOM_RIGHT_CORNER(size -> IntStream.range(0, size).filter(i -> i == size - 10 || i > size - 3).toArray()),
+    FULL(size -> IntStream.range(0, size).toArray());
+
+    private final SlotMatcher matcher;
+
+    InventoryPattern(SlotMatcher matcher) {
+        this.matcher = matcher;
+    }
+
+    public int[] getSlots(Inventory inventory) {
+        return matcher.match(inventory.getSize());
+    }
+
+    @FunctionalInterface
+    private interface SlotMatcher {
+
+        int[] match(int size);
+
+    }
+
+}
