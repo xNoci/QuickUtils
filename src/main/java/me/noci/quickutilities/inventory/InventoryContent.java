f91242d063a7b62e05caba60bc473fed0f2fb0b0
Noci <nocigit@gmail.com>
2022-09-23 16:04:20 +0200

    Added fill methods

--- src/main/java/me/noci/quickutilities/inventory/InventoryContent.java
+++ src/main/java/me/noci/quickutilities/inventory/InventoryContent.java
@@ -6,8 +6,10 @@ import org.bukkit.inventory.ItemStack;
 public class InventoryContent {
 
     private final Slot[] slots;
+    private final int size;
 
     public InventoryContent(InventoryType type, int size) {
+        this.size = size;
         this.slots = new Slot[size];
 
         for (int i = 0; i < size; i++) {
@@ -42,6 +44,47 @@ public class InventoryContent {
     public Slot getSlot(int slot) {
         if (slot > slots.length) return null; //TODO Throw exception
         return slots[slot];
+    }
+
+    /**
+     * Fill the complete inventory with the given ItemStack.
+     *
+     * @param itemStack The ItemStack that will be used
+     */
+    public void fill(ItemStack itemStack) {
+        fillPattern(itemStack, InventoryPattern.FULL);
+    }
+
+    /**
+     * Fill the border of the inventory with the given ItemStack.
+     *
+     * @param itemStack The ItemStack that will be used
+     */
+    public void fillBorders(ItemStack itemStack) {
+        fillPattern(itemStack, InventoryPattern.TOP, InventoryPattern.BOTTOM, InventoryPattern.LEFT, InventoryPattern.RIGHT);
+    }
+
+    /**
+     * Fill the corner of the inventory with the given ItemStack.
+     *
+     * @param itemStack The ItemStack that will be used
+     */
+    public void fillCorners(ItemStack itemStack) {
+        fillPattern(itemStack, InventoryPattern.TOP_LEFT_CORNER, InventoryPattern.TOP_RIGHT_CORNER, InventoryPattern.BOTTOM_LEFT_CORNER, InventoryPattern.BOTTOM_RIGHT_CORNER);
+    }
+
+    /**
+     * Fill the inventory with the given patterns.
+     *
+     * @param itemStack The ItemStack that will be used
+     * @param patterns  The patterns which will be used
+     */
+    public void fillPattern(ItemStack itemStack, InventoryPattern... patterns) {
+        for (InventoryPattern pattern : patterns) {
+            for (int slot : pattern.getSlots(this.size)) {
+                setItem(slot, itemStack);
+            }
+        }
     }
 
 
