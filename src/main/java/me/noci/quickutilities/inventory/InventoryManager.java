52b4cfb4480efaad94d309fa93ab6ee9edf61315
Noci <nocigit@gmail.com>
2022-09-23 16:38:41 +0200

    Added InventoryClickEvent

--- src/main/java/me/noci/quickutilities/inventory/InventoryManager.java
+++ src/main/java/me/noci/quickutilities/inventory/InventoryManager.java
@@ -1,14 +1,19 @@
 package me.noci.quickutilities.inventory;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class InventoryManager extends BukkitRunnable {
+public class InventoryManager extends BukkitRunnable implements Listener {
 
     public InventoryManager(JavaPlugin plugin) {
         runTaskTimer(plugin, 20, 10);
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
@@ -23,6 +28,18 @@ public class InventoryManager extends BukkitRunnable {
                     inventoryHolder.getProvider().update(player, inventoryHolder.getContent());
                     inventoryHolder.applyContent();
                 });
+    }
+
+    @EventHandler
+    public void handleInventoryClick(InventoryClickEvent event) {
+        if (!(event.getInventory().getHolder() instanceof ProvidedInventoryHolder) || event.getClickedInventory() == null)
+            return;
+        Player player = (Player) event.getWhoClicked();
+        ProvidedInventoryHolder inventoryHolder = (ProvidedInventoryHolder) event.getInventory().getHolder();
+
+
+        if (event.getClickedInventory().equals(event.getView().getBottomInventory())) return;
+        inventoryHolder.getContent().getSlot(event.getSlot()).getClickHandler().handle(player);
     }
 
 }
