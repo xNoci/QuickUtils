8c88827842f5c72fc1352e702c0c05c1de4b0b14
Noci <nocigit@gmail.com>
2022-09-23 20:10:55 +0200

    Changed TestInventory Designe

--- src/main/java/me/noci/quickutilities/TestInventory.java
+++ src/main/java/me/noci/quickutilities/TestInventory.java
@@ -11,30 +11,34 @@ import java.util.Random;
 public class TestInventory extends InventoryProvider {
 
     protected TestInventory() {
-        super("§7Test", 9 * 4);
+        super("§cGame settings", 9 * 3);
     }
 
     @Override
     public void init(Player player, InventoryContent content) {
-        content.fillBorders(new QuickItemStack(Material.STAINED_GLASS_PANE, 1, new Random().nextInt(15)).removeDisplayName());
+        content.fillBorders(new QuickItemStack(Material.STAINED_GLASS_PANE, 1, 7).removeDisplayName());
 
-        content.setItem(10, new QuickItemStack(Material.DIRT).glow().setDisplayName("Cooler Dirt :D"));
-        content.setItem(13, new QuickItemStack(Material.SKULL_ITEM, 1, 3).setSkullOwner(player.getName()).glow().setDisplayName("Cooler Head"));
-        content.setClickHandler(13, user -> user.sendMessage("Hey"));
+        content.setItem(11, new QuickItemStack(Material.INK_SACK, 1, 12).setDisplayName("§9§lDetective").setLore("", "§7Passes: §einfinity", ""));
+        content.setItem(12, new QuickItemStack(Material.INK_SACK, 1, 10).setDisplayName("§a§lInnocent").setLore("", "§7Passes: §einfinity", ""));
+        content.setItem(13, new QuickItemStack(Material.INK_SACK, 1, 1).setDisplayName("§a§lTraitor").setLore("", "§7Passes: §einfinity", ""));
+        content.setItem(15, new QuickItemStack(Material.FEATHER).setDisplayName("§eStart round").setLore("", "§7Sets the time to 10 seconds.", ""));
+        content.setItem(4, new QuickItemStack(Material.SKULL_ITEM, 1, 3).setSkullOwner(player.getName()).glow().setDisplayName("Cooler Head"));
 
-        content.addItem(new QuickItemStack(Material.COBBLE_WALL).removeDisplayName().addItemFlags(), user -> user.sendMessage("Clicked!"));
+
+        content.setClickHandler(11, user -> user.sendMessage("§f[§eTTT§f] §aYou successfully redeemed §eone detective pass §aand will be detective in this round."));
+        content.setClickHandler(12, user -> user.sendMessage("§f[§eTTT§f] §aYou successfully redeemed §eone innocent pass §aand will be innocent in this round."));
+        content.setClickHandler(13, user -> user.sendMessage("§f[§eTTT§f] §aYou successfully redeemed §eone traitor pass §aand will be traitor in this round."));
+        content.setClickHandler(15, user -> user.sendMessage("§f[§eTTT§f] §cTo accelerated the start of this game there need to be at least §e6 §cparticipants."));
     }
 
     private int step = 0;
 
     @Override
     public void update(Player player, InventoryContent content) {
-
-        if (step < 5) {
+        if (step < 20) {
             step++;
             return;
         }
-
         step = 0;
 
         content.fillBorders(new QuickItemStack(Material.STAINED_GLASS_PANE, 1, new Random().nextInt(15)).removeDisplayName());
