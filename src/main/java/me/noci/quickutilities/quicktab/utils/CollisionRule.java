bceebe61340b3250173095487d153788e91b7bfd
Noci <nocigit@gmail.com>
2022-09-24 17:22:39 +0200

    Added ID value

--- src/main/java/me/noci/quickutilities/quicktab/utils/CollisionRule.java
+++ src/main/java/me/noci/quickutilities/quicktab/utils/CollisionRule.java
@@ -1,22 +1,30 @@
 package me.noci.quickutilities.quicktab.utils;
 
+import lombok.Getter;
+
 public enum CollisionRule {
 
     /**
      * Always collide.
      */
-    ALWAYS,
+    ALWAYS("always"),
     /**
      * Only collide with other teams.
      */
-    FOR_OTHER_TEAMS,
+    FOR_OTHER_TEAMS("pushOtherTeams"),
     /**
      * Only collide with own team.
      */
-    FOR_OWN_TEAM,
+    FOR_OWN_TEAM("pushOwnTeam"),
     /**
      * Never collide.
      */
-    NEVER
+    NEVER("never");
+
+    @Getter private final String id;
+
+    CollisionRule(String id) {
+        this.id = id;
+    }
 
 }
