be7ff82f2dd40c072954c51e830f8f794bd13e7d
Noci <nocigit@gmail.com>
2022-09-25 10:10:01 +0200

    Rewritten VersionedPacketManager to VersionedTeamPacketManager

---
+++ src/main/java/me/noci/quickutilities/quicktab/packets/VersionedTeamPacketManager.java
@@ -0,0 +1,63 @@
+package me.noci.quickutilities.quicktab.packets;
+
+import com.google.common.collect.Sets;
+import com.google.common.primitives.Ints;
+import lombok.Getter;
+import me.noci.quickutilities.QuickUtils;
+import me.noci.quickutilities.quicktab.builder.TabListTeam;
+import me.noci.quickutilities.utils.ReflectionUtils;
+import org.jetbrains.annotations.ApiStatus;
+
+import java.util.List;
+import java.util.Set;
+
+@ApiStatus.Internal
+public class VersionedTeamPacketManager {
+
+    private final static PacketInfo SUPPORTED_PACKET;
+
+    static {
+        PacketInfo unknownVersion = new PacketInfo(new VersionedTeamPacketUnknown(), -1);
+
+        Set<PacketInfo> packetInfos = Sets.newHashSet();
+        packetInfos.add(new PacketInfo(new VersionedTeamPacketV1_8(), 8));
+        packetInfos.add(new PacketInfo(new VersionedTeamPacketV1_9(), 9, 10, 11, 12));
+        packetInfos.add(new PacketInfo(new VersionedTeamPacketV1_13(), 13, 14, 15, 16));
+        packetInfos.add(new PacketInfo(new VersionedTeamPacketV1_17(), 17, 18, 19));
+
+        int currentVersion = ReflectionUtils.getMajorVersion();
+        SUPPORTED_PACKET = packetInfos.stream().filter(packetInfo -> packetInfo.isVersionSupported(currentVersion)).findFirst().orElse(unknownVersion);
+
+        String info = String.format("Using TabListPacket Mapping %s. Current server version: %s (%s)",
+                SUPPORTED_PACKET.handle.protocolInfo(),
+                ReflectionUtils.getVersion(),
+                ReflectionUtils.getMajorVersion());
+
+        QuickUtils.instance().getLogger().info(info);
+    }
+
+    public static Object getTeamRemovePacket(TabListTeam team) {
+        return SUPPORTED_PACKET.getHandle().removeTeamPacket(team);
+    }
+
+    public static Object getTeamCreatePacket(TabListTeam team) {
+        return SUPPORTED_PACKET.getHandle().createTeamPacket(team);
+    }
+
+    private static class PacketInfo {
+
+        @Getter private final VersionedTeamPacket handle;
+        private final List<Integer> supportedVersions;
+
+        private PacketInfo(VersionedTeamPacket handle, int... supportedVersions) {
+            this.handle = handle;
+            this.supportedVersions = Ints.asList(supportedVersions);
+        }
+
+        public boolean isVersionSupported(int version) {
+            return this.supportedVersions.contains(version);
+        }
+
+    }
+
+}
