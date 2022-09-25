57a05e097f16b0c0850bfcae5d7b9bb883fb1138
Noci <nocigit@gmail.com>
2022-09-25 13:58:53 +0200

    Added QuickTabBuilder for testing purpose

--- src/main/java/me/noci/quickutilities/QuickUtils.java
+++ src/main/java/me/noci/quickutilities/QuickUtils.java
@@ -1,7 +1,12 @@
 package me.noci.quickutilities;
 
 import me.noci.quickutilities.inventory.GuiManager;
+import me.noci.quickutilities.quicktab.QuickTab;
+import me.noci.quickutilities.quicktab.builder.QuickTabBuilder;
+import me.noci.quickutilities.quicktab.utils.CollisionRule;
+import me.noci.quickutilities.quicktab.utils.NameTagVisibility;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,14 +15,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class QuickUtils extends JavaPlugin implements Listener {
 
+    private static QuickUtils instance;
+
+    public static QuickUtils instance() {
+        return instance;
+    }
+
     TestInventory inventory = new TestInventory();
 
     @Override
     public void onEnable() {
+        instance = this;
+
         new GuiManager(this);
 
         PluginManager pluginManager = getServer().getPluginManager();
         pluginManager.registerEvents(this, this);
+
+        QuickTabBuilder tabBuilder = QuickTab.builder()
+                .prefix((player, target) -> "§7Test §8| ")
+                .suffix((player, target) -> " §f[-]")
+                .collisionRule((player, target) -> CollisionRule.NEVER)
+                .seeFriendlyInvisible((player, target) -> true)
+                .nameTagVisibility((player, target) -> NameTagVisibility.HIDE_FOR_OWN_TEAM)
+                .color((player, target) -> ChatColor.RED);
+        QuickTab.setUpdatingTabList(this, tabBuilder);
     }
 
 
