864c8a92c1bbd74544383fb38b8afe1e3e7ca86e
Noci <nocigit@gmail.com>
2023-06-25 18:34:33 +0200

    Added default format keys

--- src/main/java/me/noci/quickutilities/utils/formatter/FormatKey.java
+++ src/main/java/me/noci/quickutilities/utils/formatter/FormatKey.java
@@ -1,6 +1,19 @@
 package me.noci.quickutilities.utils.formatter;
 
+import org.bukkit.entity.Player;
+
 public interface FormatKey {
+
+    FormatKey MESSAGE = new DefaultKey("%message%");
+    FormatKey PLAYER_NAME = new DefaultKey("%player_name%") {
+        @Override
+        public String replace(String message, Object o) {
+            if (o instanceof Player player) {
+                return replace(message, player.getName());
+            }
+            return super.replace(message, o);
+        }
+    };
 
     String getKey();
 
@@ -10,6 +23,25 @@ public interface FormatKey {
 
     default String replace(String message, String value) {
         return message.replace(getKey(), value);
+    }
+
+    class DefaultKey implements FormatKey {
+
+        private final String key;
+
+        private DefaultKey(String key) {
+            this.key = key;
+        }
+
+        @Override
+        public String getKey() {
+            return this.key;
+        }
+
+        @Override
+        public String toString() {
+            return this.key;
+        }
     }
 
 }
