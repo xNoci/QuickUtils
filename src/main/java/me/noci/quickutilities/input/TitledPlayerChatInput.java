404909bf51413572a243c520853c74a001c1d696
Noci <nocigit@gmail.com>
2023-06-25 16:58:44 +0200

    Added fadeOut and used refelction (XSeries)

--- src/main/java/me/noci/quickutilities/input/TitledPlayerChatInput.java
+++ src/main/java/me/noci/quickutilities/input/TitledPlayerChatInput.java
@@ -1,5 +1,7 @@
 package me.noci.quickutilities.input;
 
+import com.cryptomorin.xseries.messages.Titles;
+import me.noci.quickutilities.utils.BukkitUnit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -8,6 +10,9 @@ import java.util.function.Consumer;
 
 public class TitledPlayerChatInput extends PlayerChatInput {
 
+    //TODO Add constructor parameter
+    private final boolean fadeOut = false;
+    private final int fadeOutTime = (int) (BukkitUnit.SECONDS.toTicks(1) / 2);
 
     public TitledPlayerChatInput(JavaPlugin plugin, Player player, Consumer<String> playerInput, String title) {
         this(plugin, player, playerInput, title, "§7Type '§c§o%s§r§7' to abort.".formatted(CANCEL_STRING));
@@ -23,15 +28,18 @@ public class TitledPlayerChatInput extends PlayerChatInput {
         new BukkitRunnable() {
             @Override
             public void run() {
-                if(!isInputMode()) {
-                    //TODO FADE OUT
-                    player.sendTitle(" ", " "); /*TODO USING REFLECTION*/
+                if (!isInputMode()) {
+                    //TODO TEST
+                    if (fadeOut) {
+                        Titles.sendTitle(player, 0, 10, fadeOutTime, title, subtitle);
+                    } else {
+                        Titles.clearTitle(player);
+                    }
                     cancel();
                     return;
                 }
 
-                //TODO SEND CONSTANTLY
-                player.sendTitle(title, subtitle); /*TODO USING REFLECTION*/
+                Titles.sendTitle(player, title, subtitle);
             }
         }.runTaskTimerAsynchronously(plugin, 0, 10);
 
