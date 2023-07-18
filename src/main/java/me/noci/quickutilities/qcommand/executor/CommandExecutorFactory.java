7e3c77e34b78d565993ad64bd1026f4440d70c2f
Noci <nocigit@gmail.com>
2023-07-18 12:38:42 +0200

    Implemented create function

--- src/main/java/me/noci/quickutilities/qcommand/executor/CommandExecutorFactory.java
+++ src/main/java/me/noci/quickutilities/qcommand/executor/CommandExecutorFactory.java
@@ -3,9 +3,12 @@ package me.noci.quickutilities.qcommand.executor;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import me.noci.quickutilities.qcommand.QCommand;
+import me.noci.quickutilities.qcommand.mappings.CommandMapping;
+import me.noci.quickutilities.utils.Require;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -13,13 +16,12 @@ import java.util.Set;
 public class CommandExecutorFactory {
 
     @SuppressWarnings("unchecked")
-    public static <T extends CommandExecutor<T>> List<T> loadExecutors(Class<? extends QCommand> commandClass, Class<? extends Annotation> type, Class<T> executorType) {
+    public static <T extends CommandExecutor<T>> List<T> loadExecutors(Class<? extends QCommand> commandClass, Class<? extends Annotation> annotationType, Class<T> executorType) {
         Set<Method> methods = getClassMethods(commandClass);
         List<T> executors = Lists.newArrayList();
 
         for (Method method : methods) {
-            if (!method.isAnnotationPresent(type)) continue;
-            T executor = (T) create(method, executorType);
+            T executor = (T) create(method, annotationType, executorType);
             if (executor == null) continue;
             executors.add(executor);
         }
@@ -27,11 +29,22 @@ public class CommandExecutorFactory {
         return executors;
     }
 
-    private static <T extends CommandExecutor<T>> CommandExecutor<?> create(Method method, Class<T> executorType) {
-        //TODO
-        if (executorType.equals(FallbackCommandExecutor.class)) return new FallbackCommandExecutor();
-        if (executorType.equals(SubCommandCommandExecutor.class)) return new SubCommandCommandExecutor();
-        if (executorType.equals(CommandCommandExecutor.class)) return new CommandCommandExecutor();
+    private static <T extends CommandExecutor<T>> CommandExecutor<?> create(Method method, Class<? extends Annotation> annotationType, Class<T> executorType) {
+        if (!method.isAnnotationPresent(annotationType)) return null;
+
+        Require.checkState(() -> method.getParameterCount() > 0, "A command method has to have at least one parameter");
+
+        Parameter[] parameters = method.getParameters();
+        Require.checkState(() -> CommandMapping.isSenderType(parameters[0].getType()), "The first method parameter has to be a sender parameter");
+
+        for (int i = 1; i < parameters.length; i++) {
+            Parameter current = parameters[i];
+            Require.checkState(() -> CommandMapping.isArgumentType(current.getType()), "The parameter '%s' at index %s in %s is not a valid command parameter type".formatted(current.getName(), i, method.getName()));
+        }
+
+        if (executorType.equals(FallbackCommandExecutor.class)) return new FallbackCommandExecutor(method);
+        if (executorType.equals(SubCommandCommandExecutor.class)) return new SubCommandCommandExecutor(method);
+        if (executorType.equals(DefaultCommandExecutor.class)) return new DefaultCommandExecutor(method);
         return null;
     }
 
