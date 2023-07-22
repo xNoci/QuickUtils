86addb889c957782bcd7cd0fa8ef5b478541419b
Noci <nocigit@gmail.com>
2023-07-22 17:27:49 +0200

    Removed deprecated methods

--- src/main/java/me/noci/quickutilities/utils/Require.java
+++ src/main/java/me/noci/quickutilities/utils/Require.java
@@ -80,35 +80,11 @@ public class Require {
     /**
      * Check if a requirement is fulfilled.
      *
-     * @param requirement to test
-     * @param message     to send when the {@code requirement} is false
-     * @throws IllegalStateException Throws if {@link Requirement#test()} returns false
-     */
-    @Deprecated
-    public static void checkState(Requirement requirement, String message) throws IllegalStateException {
-        check(requirement, new IllegalStateException(message));
-    }
-
-    /**
-     * Check if a requirement is fulfilled.
-     *
      * @param requirement which has to be fulfilled
      * @param message     to send when the {@code requirement} is false
      * @throws IllegalArgumentException Throws if {@param requirement} is false
      */
     public static void checkArgument(boolean requirement, String message) throws IllegalArgumentException {
-        check(requirement, new IllegalArgumentException(message));
-    }
-
-    /**
-     * Check if a requirement is fulfilled.
-     *
-     * @param requirement to test
-     * @param message     to send when the {@code requirement} is false
-     * @throws IllegalArgumentException Throws if {@link Requirement#test()} returns false
-     */
-    @Deprecated
-    public static void checkArgument(Requirement requirement, String message) throws IllegalArgumentException {
         check(requirement, new IllegalArgumentException(message));
     }
 
@@ -124,26 +100,6 @@ public class Require {
     public static <T extends RuntimeException> void check(boolean requirement, T exception) throws T {
         if (requirement) return;
         throw exception;
-    }
-
-    /**
-     * Test if a requirement is fulfilled.
-     *
-     * @param requirement to test
-     * @param exception   which will be thrown if requirement is false
-     * @param <T>         type of the RuntimeException
-     * @throws T exception which will be thrown if {@link Requirement#test()} returns false
-     */
-    @Deprecated
-    public static <T extends RuntimeException> void check(Requirement requirement, T exception) throws T {
-        if (requirement.test()) return;
-        throw exception;
-    }
-
-    @FunctionalInterface
-    @Deprecated
-    public interface Requirement {
-        boolean test();
     }
 
 }
