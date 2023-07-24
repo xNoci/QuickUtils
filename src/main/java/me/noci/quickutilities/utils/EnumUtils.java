f1032950c41addbb13b610d65d195fdb4c4821c5
Noci <nocigit@gmail.com>
2023-07-24 11:57:23 +0200

    Fixed API issue

--- src/main/java/me/noci/quickutilities/utils/EnumUtils.java
+++ src/main/java/me/noci/quickutilities/utils/EnumUtils.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public class EnumUtils {
 
-    public static <T extends Enum<T>> T next(T value) {
+    public static <T extends Enum<?>> T next(T value) {
         int index = value.ordinal();
         T[] values = (T[]) value.getClass().getEnumConstants();
 
@@ -14,7 +14,7 @@ public class EnumUtils {
         return values[index + 1];
     }
 
-    public static <T extends Enum<T>> T previous(T value) {
+    public static <T extends Enum<?>> T previous(T value) {
         int index = value.ordinal();
         T[] values = (T[]) value.getClass().getEnumConstants();
 
@@ -22,29 +22,29 @@ public class EnumUtils {
         return values[index - 1];
     }
 
-    public static <T extends Enum<T>> T random(Class<T> enumClass) {
+    public static <T extends Enum<?>> T random(Class<T> enumClass) {
         T[] values = enumClass.getEnumConstants();
         return values[RandomUtils.random().nextInt(values.length)];
     }
 
-    public static <T extends Enum<T>> String join(String delimiter, Class<T> enumType) {
+    public static <T extends Enum<?>> String join(String delimiter, Class<T> enumType) {
         return join(delimiter, enumType.getEnumConstants());
     }
 
-    public static <T extends Enum<T>> String join(String delimiter, T[] values) {
+    public static <T extends Enum<?>> String join(String delimiter, T[] values) {
         return join(delimiter, List.of(values));
     }
 
-    public static <T extends Enum<T>> String join(String delimiter, List<T> values) {
+    public static <T extends Enum<?>> String join(String delimiter, List<T> values) {
         String[] names = values.stream().map(Enum::name).toArray(String[]::new);
         return String.join(delimiter, names);
     }
 
-    public static <T extends Enum<T>> Optional<T> getIfPresent(Class<T> enumClass, String value) {
+    public static <T extends Enum<?>> Optional<T> getIfPresent(Class<T> enumClass, String value) {
         return getIfPresent(enumClass, value, false);
     }
 
-    public static <T extends Enum<T>> Optional<T> getIfPresent(Class<T> enumClass, String value, boolean ignoreCase) {
+    public static <T extends Enum<?>> Optional<T> getIfPresent(Class<T> enumClass, String value, boolean ignoreCase) {
         return Arrays.stream(enumClass.getEnumConstants())
                 .filter(e -> ignoreCase ? e.name().equalsIgnoreCase(value) : e.name().equals(value))
                 .findFirst();
