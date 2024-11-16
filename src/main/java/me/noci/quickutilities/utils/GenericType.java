d4dbd1e00a8e7b1d8d0f7cdd5aa057d2261d2f56
Noci <nocigit@gmail.com>
2024-11-16 15:11:56 +0100

    feat: removed GenericType

--- src/main/java/me/noci/quickutilities/utils/GenericType.java
+++
@@ -1,9 +0,0 @@
-package me.noci.quickutilities.utils;
-
-import com.google.common.reflect.TypeToken;
-
-@SuppressWarnings("UnstableApiUsage")
-public abstract class GenericType<T> {
-    protected final TypeToken<T> type = new TypeToken<>(getClass()) {
-    };
-}
